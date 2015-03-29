944c29bd52d4fc5985a9e849a46debd6ebe6a885
Timothy Chin <tim@Timothys-MacBook-Pro.local>
2015-03-29 11:02:25 -0400

    utils: when / extend

--- app/src/base/Utils.js
+++ app/src/base/Utils.js
@@ -1,11 +1,34 @@
 /* globals define */
 define(function(require, exports, module) {
   'use strict';
+  var Engine = require('famous/core/Engine');
 
   module.exports = {
     inherits: function (_parent, _child) {
       _child.prototype = Object.create(_parent.prototye);
       _child.prototype.constructor = child;
+    },
+    when: function (predicate, cb) {
+      var checkPred = function () {
+        var bool = predicate();
+        if (bool) {
+          cb();
+        }
+        else {
+          Engine.nextTick(checkPred);
+        }
+      }
+      Engine.nextTick(checkPred);
+    },
+    extend: function (a /**, b.. */) {
+      var _args = Array.prototype.slice.call(arguments, 1, arguments.length)
+      for (var i = 0; i < _args.length; i++) { 
+        var obj = _args[i];
+        for (var key in obj) {
+          a[key] = obj[key];
+        }
+      }
+      return a;
     }
   };
 
