9b575ba197f7ae77266ae127c5b24c5d635e336e
Tim Chin <timjchin@gmail.com>
2015-04-21 00:28:42 -0400

    sequential layout bug fixes

--- app/src/flipButton/SequentialLayout.js
+++ app/src/flipButton/SequentialLayout.js
@@ -1,11 +1,17 @@
 /* globals define */
 define(function(require, exports, module) {
   'use strict';
-  var LayoutBase = require('../base/LayoutBase');
+  var LayoutBase = require('famous-moveable/LayoutBase');
   var Transform = require('famous/core/Transform');
 
   function SequentialLayout () {
     LayoutBase.apply(this, arguments);
+    this._direction = directionToDir(this.options.direction);
+    this._setDirection = this._setDirection.bind(this);
+  }
+
+  function directionToDir (dir) {
+    return dir === 'x' ? 0 : 1 
   }
 
   SequentialLayout.prototype = Object.create(LayoutBase.prototype);
@@ -13,50 +19,45 @@ define(function(require, exports, module) {
 
   SequentialLayout.DEFAULT_OPTIONS = {
     layoutAnimation: 'cards',
-    sequentialPadding: 15,
-    sequentialDirection: 'y'
+    padding: 15,
+    direction: 'y',
+    delay: 100
   }
 
-  SequentialLayout.animations = { 
-    cards: function (child, offset, options, i) {
-      child.setTransform(Transform.translate(0,0, i * 15));
-      child.delayByKey('transform', i * 200);
-      child.delayByKey('opacity', i * 200);
+  SequentialLayout.animations = {
+    cards: function (child, offset, options, i, cb) {
+      child.halt();
+      child.delayByKey('transform', i * options.delay);
+      child.delayByKey('opacity', i * options.delay);
       child.setOpacity(1, { duration: 700 });
       child.setTransform(Transform.translate(offset[0], offset[1], offset[2]), { 
-        curve: 'inOutBack',
+        curve: 'outExpo',
         duration: 400
-      });
+      }, cb);
     }
   }
 
   SequentialLayout.prototype.layout = function (children, sizes) {
       var offset = [0,0,0];
-      var dir = this.options.sequentialDirection == 'x' ? 0 : 1;
-      for (var i = 0; i < children.length; i++) { 
+      var dir = directionToDir(this.options.direction);
+      for (var i = 0, len = children.length; i < len; i++) { 
         var child = children[i];
+        var cb = i === len-1 ? this._setDirection : undefined
 
         SequentialLayout.animations[this.options.layoutAnimation](
           child,
           offset,
           this.options,
-          i
+          i,
+          cb
         );
 
-        offset[dir] += sizes[i][dir] + this.options.sequentialPadding;
+        offset[dir] += sizes[i][dir] + this.options.padding;
       }
   }
 
-  SequentialLayout.prototype.getSize = function (children, sizes) {
-    var totalSize = [0,0];
-    var dir = this.options.sequentialDirection == 'x' ? 0 : 1;
-    var oppositeDir = dir === 0 ? 1 : 0;
-    for (var i = 0; i < sizes.length; i++) { 
-      var childSize = sizes[i];
-      totalSize[dir] += childSize[dir] + this.options.sequentialPadding;
-      totalSize[oppositeDir] = Math.max(childSize[oppositeDir], totalSize[oppositeDir]);
-    }
-    return totalSize;
+  SequentialLayout.prototype._setDirection = function () {
+    this._direction = directionToDir(this.options.direction);
   }
 
   module.exports = SequentialLayout;
