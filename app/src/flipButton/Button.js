be00e27db615c6ba74cb77f25302b948bd5c8a49
Tim Chin <timjchin@gmail.com>
2015-04-21 00:29:27 -0400

    button sizing

--- app/src/flipButton/Button.js
+++ app/src/flipButton/Button.js
@@ -2,19 +2,21 @@
 define(function(require, exports, module) {
   'use strict';
 
-  var MoveableView = require('../base/MoveableView');
-  var MoveableSurface = require('../base/MoveableSurface');
+  var MoveableView = require('famous-moveable/MoveableView');
+  var MoveableSurface = require('famous-moveable/MoveableSurface');
   var Transform = require('famous/core/Transform');
-  var Utils = require('../base/Utils');
+  var Utils = require('famous-moveable/Utils');
 
   function Button (options) {
     MoveableView.apply(this, arguments);
 
     this._button = new MoveableSurface(
       MoveableSurface.getPassedOptions(this, 'button'));
+    this._boundResize = this.emitResize.bind(this);
 
     this.onClick = this.onClick.bind(this);
     this.bindEvents();
+    this.setSize(this.getSize.bind(this));
 
     this.add(this._button);
   }
@@ -25,31 +27,27 @@ define(function(require, exports, module) {
   Button.prototype.getSize = function () {
     return this._button.getSize();
   }
-  Button.prototype.addListener = function () {
-    return this._button.on.apply(this._button, arguments);
+
+  Button.prototype.emitResize = function () {
+    this.emit('resize');
   }
-
-  Button.prototype.on = Button.prototype.addListener;
-
-  Button.prototype.removeListener = function () {
-    return this._button.removeListener.apply(this._button, arguments);
-  }
-
-  Button.prototype.off = Button.prototype.removeListener;
 
   Button.prototype.bindEvents = function () {
     this._button.addListener('mousedown', this.onClick);
     this._button.addListener('touchend', this.onClick);
+    this._button.on('resize', this._boundResize);
   }
 
   Button.prototype.unbindEvents = function () {
     this._button.removeListener('mousedown', this.onClick);
     this._button.removeListener('touchend', this.onClick);
+    this._button.removeListener('resize', this._boundResize);
   }
 
   Button.prototype.onClick = function () {
     Button.animations.onClick[this.options.clickAnimation].call(this, this._button);
     if (this.options.event) this.emit(this.options.event);
+    this.emit('selected');
   }
 
   Button.prototype.getEvent = function () {
