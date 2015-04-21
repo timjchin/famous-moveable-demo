c49d5313731088a74c9eeff1c2e2c1f48911816b
Tim Chin <timjchin@gmail.com>
2015-04-21 00:31:36 -0400

    second scene, hover button options

--- app/src/two.js
+++ app/src/two.js
@@ -1,21 +1,29 @@
 define(function(require, exports, module) {
   'use strict';
-  var MoveableView = require('./base/MoveableView');
-  var MoveableSurface = require('./base/MoveableSurface');
+  var MoveableView = require('famous-moveable/MoveableView');
+  var MoveableSurface = require('famous-moveable/MoveableSurface');
   var Transform = require('famous/core/Transform');
   var Button = require('./flipButton/Button');
+  var Engine = require('famous/core/Engine');
+  var RegisterCurves = require('famous-moveable/RegisterCurves');
+  var Button = require('./flipButton/Button');
+  var Modifier = require('famous/core/Modifier');
+  var Surface= require('famous/core/Surface');
+  var HoverButton = require('./flipButton/HoverButton');
+  var Dropdown = require('./flipButton/Dropdown');
+  var Layout = require('famous-moveable/Layout');
+  var SequentialLayout = require('./flipButton/SequentialLayout');
 
-  function SceneTwo () {
+  function ButtonScene () {
     MoveableView.apply(this, arguments);
-    document.body.classList.add('scene-two');
 
-    var textButton = new Button({ 
+    var textButton = new HoverButton({ 
       buttonContent: '<i class="h1 button fa fa-align-left"></i>',
       textContent: '<h6 class="text-label">Left Align</h6>',
       origin: [0.5, 0.5],
       align: [0.5, 0.5],
+      show: 'fade',
       opacity: 0,
-      transform: Transform.rotateY(Math.PI)
     });
 
     textButton
@@ -28,10 +36,73 @@ define(function(require, exports, module) {
       });
 
     this.add(textButton);
+
+    var showAnimation = new Dropdown({
+      children: [
+        new Button({ 
+          buttonContent: '<div class="dropdown-button">Fade</div>',
+          event: 'fade'
+        }),       
+        new Button({ 
+          buttonContent: '<div class="dropdown-button">Move Down</div>',
+          event: 'moveDown'
+        }),       
+        new Button({ 
+          buttonContent: '<div class="dropdown-button">Rotate Down</div>',
+          event: 'rotateDown'
+        }),       
+      ]
+    });
+    showAnimation.on('selected', function (e) {
+      textButton.setOptions({
+        show: e
+      });
+    });
+
+    var hideAnimation = new Dropdown({
+      children: [
+        new Button({ 
+          buttonContent: '<div class="dropdown-button">Fade</div>',
+          event: 'fade'
+        }),       
+        new Button({ 
+          buttonContent: '<div class="dropdown-button">Rotate Out</div>',
+          event: 'rotateOut'
+        }),       
+      ]
+    });
+
+    hideAnimation.on('selected', function (e) {
+      textButton.setOptions({
+        hide: e
+      });
+    });
+
+    var dropdownLayout = new Layout({
+      children: [
+        showAnimation,
+        hideAnimation
+      ],
+      layout: new SequentialLayout({
+        direction: 'x',
+        padding: 15
+      }),
+      origin: [1, 0],
+      align: [0.98, 0.05]
+    });
+    
+
+    this.add(dropdownLayout);
   }
 
-  SceneTwo.prototype = Object.create(MoveableView.prototype);
-  SceneTwo.prototype.constructor = SceneTwo;
+  ButtonScene.prototype = Object.create(MoveableView.prototype);
+  ButtonScene.prototype.constructor = ButtonScene;
 
-  module.exports = SceneTwo;
+  // create the main context
+  var mainContext = Engine.createContext();
+  mainContext.setPerspective(1000);
+  // chrome bug, ensure that it applies perspective.
+  mainContext.container.style.backfaceVisibility = 'hidden';
+  mainContext.add(new ButtonScene());
+
 });
